4d960f505e87e1fa5d162d6c29005a00a7e82b80
GenesisDaniela <genesisdanielavjau@ufps.edu.co>
2022-09-18 23:17:02 -0500

    Uri change

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  // URL = "https://portfoliogv.herokuapp.com/"
-  URL = "http://127.0.0.1:5000/"
+  URL = "https://portfoliogv.herokuapp.com/"
+  // URL = "http://127.0.0.1:5000/"
 
   // URL = "https://portfoliogv.herokuapp.com/api/"
   constructor(private http:HttpClient) { }
